ac14dbe9d8c468cc26aa0dafa5663c22530c66e8
OKUNOKENTARO <okunokentaro@me.com>
2015-07-07 16:51:51 +0900

    feat(cw-user-agent): implement setUA()

--- src/cw-user-agent.js
+++ src/cw-user-agent.js
@@ -2,4 +2,11 @@ export default class Parser {
   constructor() {
     // noop
   }
+
+  /**
+   * @param {string} ua - user agent
+   */
+  setUA(ua) {
+    this.ua = ua;
+  }
 }
--- test/cw-user-agent-test.js
+++ test/cw-user-agent-test.js
@@ -1,5 +1,13 @@
 import assert from 'assert';
+import Parser from '../index';
+const parser = new Parser();
 
-it('test', () => {
-  assert.equal(1, 3);
+describe('Parser', () => {
+  describe('setUA', () => {
+    it('should be set the user agent', () => {
+      const ua = 'Dummy';
+      parser.setUA(ua);
+      assert.equal(parser.ua, ua);
+    });
+  });
 });
