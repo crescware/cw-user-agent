1dc2fccb940320b3b2b633b9eccfbb6e7c91b9d0
OKUNOKENTARO <okunokentaro@me.com>
2015-07-07 20:42:44 +0900

    chore(cw-user-agent): declare regexp

--- src/cw-user-agent.js
+++ src/cw-user-agent.js
@@ -1,3 +1,23 @@
+const re = {
+  apple: {
+    phone:  /iPhone/i,
+    iPod:   /iPod/i,
+    tablet: /iPad/i
+  },
+  android: {
+    phone:  /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,
+    tablet: /Android/i
+  },
+  windows: {
+    phone:  /IEMobile/i,
+    tablet: /(?=.*\bWindows\b)(?=.*\bARM\b)/i
+  },
+  blackberry:    /BlackBerry/i,
+  blackberry10:  /BB10/i,
+  operaMini:     /Opera Mini/i,
+  firefoxMobile: /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i
+};
+
 export default class Parser {
   constructor() {
     // noop
