9c98160d30a4d0afe931323048811617f2ecb4e1
OKUNOKENTARO <okunokentaro@me.com>
2015-07-08 00:32:46 +0900

    refactor(cw-user-agent): carve out to android-device-info.js

---
+++ src/android-device-info.js
@@ -0,0 +1,75 @@
+import re from './regexp';
+import DeviceInfo from './device-info';
+import {match} from './match-utils';
+
+export default class AndroidDeviceInfo extends DeviceInfo {
+  constructor(ua) {
+    super(ua);
+  }
+
+  /**
+   * @returns {cwua.BrowserInfo}
+   */
+  get browser() {
+    const androidBrowser = this.ua.match(/android.+version\/([\d\.]+)\s+(?:mobile\s?safari|safari)/i);
+
+    if (androidBrowser) {
+      const version = androidBrowser[1];
+      return {
+        name:    'Android Browser',
+        version: version,
+        major:   parseInt(version.split('.')[0], 10)
+      };
+    }
+
+    const chrome = this.ua.match(/\bChrome\/([\d\.]+)\s/);
+
+    return {
+      name:    'Chrome',
+      version: chrome[1],
+      major:   parseInt(chrome[1].split('.')[0], 10)
+    };
+  }
+
+  /**
+   * @returns {cwua.EngineInfo}
+   */
+  get engine() {
+    const version = this.ua.match(/\bAppleWebKit\/(\d+\.\d+(\.\d+)?)/)[1];
+
+    return {
+      name:    'WebKit',
+      version: version
+    };
+  }
+
+  /**
+   * @returns {cwua.OsInfo}
+   */
+  get os() {
+    const raw = this.ua.match(/\bAndroid\b\s(\d\.\d(\.\d)?);/)[1];
+    const verArr = raw.split('.');
+
+    const osInfo = {
+      name:    'Android',
+      version: verArr.join('.'),
+      major:   parseInt(verArr[0], 10),
+      minor:   parseInt(verArr[1], 10)
+    };
+    if (verArr[2] !== void 0 && verArr[2] !== null) {
+      osInfo.patch = parseInt(verArr[2], 10);
+    }
+
+    return osInfo;
+  }
+
+  /**
+   * @returns {string}
+   */
+  /* eslint-disable no-multi-spaces, complexity */
+  get device() {
+    if (match(re.android.phone,  this.ua)) { return 'phone'; }
+    if (match(re.android.tablet, this.ua)) { return 'tablet'; }
+  }
+  /* eslint-enable no-multi-spaces, complexity */
+}
--- src/cw-user-agent.js
+++ src/cw-user-agent.js
@@ -1,79 +1,8 @@
 import re from './regexp';
 import DeviceInfo from './device-info';
 import AppleDeviceInfo from './apple-device-info';
+import AndroidDeviceInfo from './android-device-info';
 import {match, isApple, isAndroid, isWindows} from './match-utils';
-
-class AndroidDeviceInfo extends DeviceInfo {
-  constructor(ua) {
-    super(ua);
-  }
-
-  /**
-   * @returns {cwua.BrowserInfo}
-   */
-  get browser() {
-    const androidBrowser = this.ua.match(/android.+version\/([\d\.]+)\s+(?:mobile\s?safari|safari)/i);
-
-    if (androidBrowser) {
-      const version = androidBrowser[1];
-      return {
-        name:    'Android Browser',
-        version: version,
-        major:   parseInt(version.split('.')[0], 10)
-      };
-    }
-
-    const chrome = this.ua.match(/\bChrome\/([\d\.]+)\s/);
-
-    return {
-      name:    'Chrome',
-      version: chrome[1],
-      major:   parseInt(chrome[1].split('.')[0], 10)
-    };
-  }
-
-  /**
-   * @returns {cwua.EngineInfo}
-   */
-  get engine() {
-    const version = this.ua.match(/\bAppleWebKit\/(\d+\.\d+(\.\d+)?)/)[1];
-
-    return {
-      name:    'WebKit',
-      version: version
-    };
-  }
-
-  /**
-   * @returns {cwua.OsInfo}
-   */
-  get os() {
-    const raw = this.ua.match(/\bAndroid\b\s(\d\.\d(\.\d)?);/)[1];
-    const verArr = raw.split('.');
-
-    const osInfo = {
-      name:    'Android',
-      version: verArr.join('.'),
-      major:   parseInt(verArr[0], 10),
-      minor:   parseInt(verArr[1], 10)
-    };
-    if (verArr[2] !== void 0 && verArr[2] !== null) {
-      osInfo.patch = parseInt(verArr[2], 10);
-    }
-
-    return osInfo;
-  }
-
-  /**
-   * @returns {string}
-   */
-  /* eslint-disable no-multi-spaces, complexity */
-  get device() {
-    if (match(re.android.phone,  this.ua)) { return 'phone'; }
-    if (match(re.android.tablet, this.ua)) { return 'tablet'; }
-  }
-  /* eslint-enable no-multi-spaces, complexity */
-}
 
 class WindowsDeviceInfo extends DeviceInfo {
   constructor(ua) {
